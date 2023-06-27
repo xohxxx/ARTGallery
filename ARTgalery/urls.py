cb69308cfc1a1d2293957d1c3bd891b3425c7c8b
spkeer <tomasignacio.bv@gmail.com>
2023-06-27 21:11:24 +0200

    camilogay
    asd

--- ARTgalery/urls.py
+++ ARTgalery/urls.py
@@ -16,8 +16,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from alumnos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alumnos/',include('alumnos.urls'))
+    path('alumnos/',include('alumnos.urls')),
+    path('index/', views.index, name='index'),
+    path('adminuser/', views.adminuser, name='adminuser'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('feriado/', views.feriado, name='feriado'),
+    path('formadmin/', views.formadmin, name='formadmin'),
+    path('formulario/', views.formulario, name='formulario'),
+    path('galeria/', views.galeria, name='galeria'),
+    path('login/', views.login, name='login'),
+    path('pintura1/', views.pintura1, name='pintura1'),
+    path('pintura2/', views.pintura2, name='pintura2'),
+    path('pintura3/', views.pintura3, name='pintura3'),
+    path('registroadmin/', views.registroadmin, name='registroadmin'),
 ]
