85a4db1f900a0db1a69f7489774d4ee5f5fe142a
spkeer <tomasignacio.bv@gmail.com>
2023-06-28 07:40:49 +0200

    2.0
    se hizo lo que se pudo

--- alumnos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Administrador, Artista, Contacto, Sexo
-# Register your models here.
-admin.site.register(Administrador)
-admin.site.register(Artista)
-admin.site.register(Contacto)
-admin.site.register(Sexo)
-
